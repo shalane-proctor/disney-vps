f2aab4604b6802649d406a4c7729e12bf9975a7c
Shalane Proctor <shalane088@gmail.com>
2022-08-31 00:45:39 -0500

    added vp api calls, added vp cards, update index, updated ReadME

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,46 @@
-import { signOut } from '../utils/auth';
+import { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
+import { getVps } from '../api/VPData';
+import VPCards from '../components/VPCard';
 import { useAuth } from '../utils/context/authContext';
 
 function Home() {
   const { user } = useAuth();
+  const [attendees, setAttendees] = useState([]);
+  const getAttendees = () => {
+    getVps().then(setAttendees);
+  };
+  useEffect(() => {
+    getAttendees();
+  }, []);
 
   return (
     <div
       className="text-center d-flex flex-column justify-content-center align-content-center"
       style={{
-        height: '90vh',
         padding: '30px',
-        maxWidth: '400px',
+        maxWidth: '1000px',
         margin: '0 auto',
       }}
     >
-      <h1>Hello {user.displayName}! </h1>
-      <p>Click the button below to logout!</p>
-      <button className="btn btn-danger btn-lg copy-btn" type="button" onClick={signOut}>
-        Sign Out
-      </button>
+      <p>Thank you cast member {user.displayName} for signing in to take attendance</p>
+      <h1>Disney&apos;s Vice President economics discussion Panel</h1>
+      <h3>Please fill out form below to check in</h3>
+      <Button
+        style={{
+          maxWidth: '400px',
+          margin: '0 auto',
+        }}
+        className="btn btn-dark btn-lg copy-btn"
+        type="button"
+      >
+        Check-in
+      </Button>
+      {attendees.map((attendee) => (
+        <VPCards key={attendee.firebaseKey} vpObj={attendee} onUpdate={getAttendees} />
+      ))}
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img src="https://logos-world.net/imageup/Disney_World/Disney_World_(6).png" width="915" alt="Disney Logo" />
     </div>
   );
 }
